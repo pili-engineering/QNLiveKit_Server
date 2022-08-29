5f9bf26a05fcde883ea4b2d62933587e2f653b22
Y-SYN <1600852289@qq.com>
2022-08-29 18:19:39 +0800

    authTokenAdmin

--- biz/admin/service.go
+++ biz/admin/service.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"context"
+	"errors"
 	"github.com/qbox/livekit/biz/model"
 	"github.com/qbox/livekit/common/api"
 	"github.com/qbox/livekit/common/mysql"
@@ -11,6 +12,7 @@ import (
 type ManagerService interface {
 	FindAdminByUserName(ctx context.Context, userName string) (*model.ManagerEntity, error)
 	FindAdminByUserId(ctx context.Context, userId string) (*model.ManagerEntity, error)
+	FindOrCreateAdminUser(ctx context.Context, userId string) (*model.ManagerEntity, error)
 }
 
 type ManService struct {
@@ -50,4 +52,21 @@ func (s *ManService) FindAdminByUserId(ctx context.Context, userId string) (*mod
 		}
 	}
 	return me, nil
+}
+
+func (s *ManService) FindOrCreateAdminUser(ctx context.Context, userId string) (*model.ManagerEntity, error) {
+	log := logger.ReqLogger(ctx)
+	entity, err := s.FindAdminByUserId(ctx, userId)
+	if err != nil && errors.Is(err, api.ErrNotFound) {
+		db := mysql.GetLive(log.ReqID())
+		m := &model.ManagerEntity{
+			UserId: userId,
+		}
+		err = db.Model(model.ManagerEntity{}).Save(m).Error
+		return m, err
+	} else if err != nil {
+		return nil, err
+	} else {
+		return entity, nil
+	}
 }
