bdcc582d69b1c78988268d3bcaed77d952c7f586
wangsheng <wangsheng@qiniu.com>
2022-08-30 14:22:12 +0800

    SOL-305 管理员操作增加操作日志 (#16)
    * 管理员操作日志记录
    
    * 管理员操作日志记录

--- app/live/internal/controller/admin/route.go
+++ app/live/internal/controller/admin/route.go
@@ -3,12 +3,13 @@ package admin
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/qbox/livekit/app/live/internal/config"
+	"github.com/qbox/livekit/app/live/internal/middleware"
 	"github.com/qbox/livekit/common/auth/liveauth"
 )
 
 func RegisterRoute(engine *gin.Engine) {
 	group := engine.Group("/manager")
 	group.GET("/login", censorController.LoginManager)
-	adminGroup := engine.Group("/admin", liveauth.AuthAdminHandleFunc(config.AppConfig.JwtKey))
+	adminGroup := engine.Group("/admin", liveauth.AuthAdminHandleFunc(config.AppConfig.JwtKey), middleware.OperatorLogMiddleware())
 	RegisterCensorRoutes(adminGroup)
 }
