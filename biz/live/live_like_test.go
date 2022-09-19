394e507c352faa42216d96f6ec77d33fda0c5daf
wangsheng <wangsheng@qiniu.com>
2022-09-19 18:03:56 +0800

    test

--- biz/live/live_like_test.go
+++ biz/live/live_like_test.go
@@ -12,104 +12,153 @@ import (
 )
 
 func TestService_cacheLikeRoomUsers(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		now    time.Time
-		liveId string
-		userId string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{}
-			tt.wantErr(t, s.cacheLikeRoomUsers(tt.args.ctx, tt.args.now, tt.args.liveId, tt.args.userId), fmt.Sprintf("cacheLikeRoomUsers(%v, %v, %v, %v)", tt.args.ctx, tt.args.now, tt.args.liveId, tt.args.userId))
-		})
-	}
+	likeCacheSetup()
+	defer likeCacheTearDown()
+
+	ctx := context.Background()
+	now := time.Now()
+	liveId1 := fmt.Sprintf("live_%d_1", now.Unix())
+	liveId2 := fmt.Sprintf("live_%d_2", now.Unix())
+
+	userId1 := "user_1"
+	userId2 := "user_2"
+	userId3 := "user_3"
+
+	s := &Service{}
+	s.cacheLikeRoomUsers(ctx, now, liveId1, userId1)
+	s.cacheLikeRoomUsers(ctx, now, liveId1, userId2)
+	s.cacheLikeRoomUsers(ctx, now, liveId1, userId3)
+	s.cacheLikeRoomUsers(ctx, now, liveId1, userId1)
+
+	s.cacheLikeRoomUsers(ctx, now, liveId2, userId1)
+	s.cacheLikeRoomUsers(ctx, now, liveId2, userId2)
+	s.cacheLikeRoomUsers(ctx, now, liveId2, userId1)
+
+	users, err := s.getLikeRoomUsers(ctx, now, liveId1)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, len(users))
+
+	users, err = s.getLikeRoomUsers(ctx, now, liveId2)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(users))
 }
 
 func TestService_cacheLikeRooms(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		now    time.Time
-		liveId string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{}
-			tt.wantErr(t, s.cacheLikeRooms(tt.args.ctx, tt.args.now, tt.args.liveId), fmt.Sprintf("cacheLikeRooms(%v, %v, %v)", tt.args.ctx, tt.args.now, tt.args.liveId))
-		})
-	}
+	likeCacheSetup()
+	defer likeCacheTearDown()
+
+	ctx := context.Background()
+	now := time.Now()
+	liveId1 := fmt.Sprintf("live_%d_1", now.Unix())
+	liveId2 := fmt.Sprintf("live_%d_2", now.Unix())
+
+	s := &Service{}
+	err := s.cacheLikeRooms(ctx, now, liveId1)
+	assert.Nil(t, err)
+	err = s.cacheLikeRooms(ctx, now, liveId2)
+	assert.Nil(t, err)
+
+	rooms, err := s.getLikeRooms(ctx, now)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(rooms))
+
+	err = s.cacheLikeRooms(ctx, now, liveId2)
+	assert.Nil(t, err)
+
+	rooms, err = s.getLikeRooms(ctx, now)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(rooms))
 }
 
 func TestService_incrRoomLikes(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		liveId string
-		userId string
-		count  int64
-	}
-	tests := []struct {
-		name      string
-		args      args
-		wantMy    int64
-		wantTotal int64
-		wantErr   assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{}
-			gotMy, gotTotal, err := s.incrRoomLikes(tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-			if !tt.wantErr(t, err, fmt.Sprintf("incrRoomLikes(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)) {
-				return
-			}
-			assert.Equalf(t, tt.wantMy, gotMy, "incrRoomLikes(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-			assert.Equalf(t, tt.wantTotal, gotTotal, "incrRoomLikes(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-		})
-	}
+	likeCacheSetup()
+	defer likeCacheTearDown()
+
+	ctx := context.Background()
+	now := time.Now()
+	liveId1 := fmt.Sprintf("live_%d_1", now.Unix())
+	liveId2 := fmt.Sprintf("live_%d_2", now.Unix())
+
+	userId1 := "user_1"
+	userId2 := "user_2"
+	userId3 := "user_3"
+
+	s := &Service{}
+	my, total, err := s.incrRoomLikes(ctx, liveId1, userId1, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(1), total)
+
+	my, total, _ = s.incrRoomLikes(ctx, liveId1, userId1, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(3), my)
+	assert.Equal(t, int64(3), total)
+
+	my, total, _ = s.incrRoomLikes(ctx, liveId1, userId2, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(4), total)
+
+	my, total, _ = s.incrRoomLikes(ctx, liveId1, userId2, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(3), my)
+	assert.Equal(t, int64(6), total)
+
+	my, total, _ = s.incrRoomLikes(ctx, liveId2, userId2, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(1), total)
+
+	my, total, _ = s.incrRoomLikes(ctx, liveId2, userId3, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(2), my)
+	assert.Equal(t, int64(3), total)
+
 }
 
 func TestService_cacheLike(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		liveId string
-		userId string
-		count  int64
-	}
-	tests := []struct {
-		name      string
-		args      args
-		wantMy    int64
-		wantTotal int64
-		wantErr   assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{}
-			gotMy, gotTotal, err := s.cacheLike(tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-			if !tt.wantErr(t, err, fmt.Sprintf("cacheLike(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)) {
-				return
-			}
-			assert.Equalf(t, tt.wantMy, gotMy, "cacheLike(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-			assert.Equalf(t, tt.wantTotal, gotTotal, "cacheLike(%v, %v, %v, %v)", tt.args.ctx, tt.args.liveId, tt.args.userId, tt.args.count)
-		})
-	}
+	likeCacheSetup()
+	defer likeCacheTearDown()
+
+	ctx := context.Background()
+	now := time.Now()
+	liveId1 := fmt.Sprintf("live_%d_1", now.Unix())
+	liveId2 := fmt.Sprintf("live_%d_2", now.Unix())
+
+	userId1 := "user_1"
+	userId2 := "user_2"
+	userId3 := "user_3"
+
+	s := &Service{}
+	my, total, err := s.cacheLike(ctx, liveId1, userId1, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(1), total)
+
+	my, total, _ = s.cacheLike(ctx, liveId1, userId1, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(3), my)
+	assert.Equal(t, int64(3), total)
+
+	my, total, _ = s.cacheLike(ctx, liveId1, userId2, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(4), total)
+
+	my, total, _ = s.cacheLike(ctx, liveId1, userId2, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(3), my)
+	assert.Equal(t, int64(6), total)
+
+	my, total, _ = s.cacheLike(ctx, liveId2, userId2, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), my)
+	assert.Equal(t, int64(1), total)
+
+	my, total, _ = s.cacheLike(ctx, liveId2, userId3, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(2), my)
+	assert.Equal(t, int64(3), total)
 }
 
 func likeCacheSetup() {
