b3752bef3608f6341df0b6db3b37d4c235b8b7e9
Y-SYN <36581904+Y-SYN@users.noreply.github.com>
2022-09-22 14:26:45 +0800

    SQL-322-2  gift配置 (#33)

--- biz/gift/service.go
+++ biz/gift/service.go
@@ -12,8 +12,8 @@ import (
 
 type GService interface {
 	SaveGiftEntity(context context.Context, entity *model.GiftEntity) error
-	DeleteGiftEntity(context context.Context, typeId int) error
-	GetListGiftEntity(context context.Context) ([]*model.GiftEntity, error)
+	DeleteGiftEntity(context context.Context, giftId int) error
+	GetListGiftEntity(context context.Context, typeId int) ([]*model.GiftEntity, error)
 }
 
 type Service struct {
@@ -28,11 +28,15 @@ func GetService() GService {
 func (s *Service) SaveGiftEntity(context context.Context, entity *model.GiftEntity) error {
 	log := logger.ReqLogger(context)
 	db := mysql.GetLive(log.ReqID())
-	sql := "insert into gift_config(type,name,amount,img,animation_type,animation_img,`order`,extends, created_at,updated_at) " +
-		"VALUES(?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE type = ?,name = ?,amount = ?," +
+	sql := "insert into gift_config(gift_id,type,name," +
+		"amount,img,animation_type,animation_img," +
+		"`order`,extends, created_at, updated_at)  " +
+		"VALUES(?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE gift_id = ?,type = ?,name = ?,amount = ?," +
 		"img = ?,animation_type = ?,animation_img=?,`order`=?,extends=?, created_at=?,updated_at=?,deleted_at = NULL"
-	err := db.Exec(sql, entity.Type, entity.Name, entity.Amount, entity.Img, entity.AnimationType, entity.AnimationImg, entity.Order, entity.Extends, time.Now(), time.Now(),
-		entity.Type, entity.Name, entity.Amount, entity.Img, entity.AnimationType, entity.AnimationImg, entity.Order, entity.Extends, time.Now(), time.Now()).Error
+	err := db.Exec(sql, entity.GiftId, entity.Type, entity.Name,
+		entity.Amount, entity.Img, entity.AnimationType, entity.AnimationImg,
+		entity.Order, entity.Extends, time.Now(), time.Now(),
+		entity.GiftId, entity.Type, entity.Name, entity.Amount, entity.Img, entity.AnimationType, entity.AnimationImg, entity.Order, entity.Extends, time.Now(), time.Now()).Error
 	if err != nil {
 		log.Errorf("add gift config error %s", err.Error())
 		return api.ErrDatabase
@@ -40,10 +44,10 @@ func (s *Service) SaveGiftEntity(context context.Context, entity *model.GiftEnti
 	return nil
 }
 
-func (s *Service) DeleteGiftEntity(context context.Context, typeId int) error {
+func (s *Service) DeleteGiftEntity(context context.Context, giftId int) error {
 	log := logger.ReqLogger(context)
 	db := mysql.GetLive(log.ReqID())
-	err := db.Delete(&model.GiftEntity{}, "type = ?", typeId).Error
+	err := db.Delete(&model.GiftEntity{}, "gift_id = ?", giftId).Error
 	if err != nil {
 		log.Errorf("delete gift config error %s", err.Error())
 		return api.ErrDatabase
@@ -51,11 +55,11 @@ func (s *Service) DeleteGiftEntity(context context.Context, typeId int) error {
 	return nil
 }
 
-func (s *Service) GetListGiftEntity(context context.Context) ([]*model.GiftEntity, error) {
+func (s *Service) GetListGiftEntity(context context.Context, typeId int) ([]*model.GiftEntity, error) {
 	log := logger.ReqLogger(context)
 	db := mysql.GetLive(log.ReqID())
 	entities := make([]*model.GiftEntity, 0)
-	err := db.Model(&model.GiftEntity{}).Order("`order` asc").Order("created_at desc").Find(&entities).Error
+	err := db.Model(&model.GiftEntity{}).Where("type = ?", typeId).Order("`order` asc").Order("created_at desc").Find(&entities).Error
 	if err != nil {
 		log.Errorf("list gift config error %s", err.Error())
 		return nil, api.ErrDatabase
