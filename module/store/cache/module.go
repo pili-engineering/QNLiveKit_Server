00588489a66a8aa2f6154dd513eff8a90fcb6ab2
wangsheng <wangsheng@qiniu.com>
2022-09-23 15:25:01 +0800

    mysql, cache 模块化

--- module/store/cache/module.go
+++ module/store/cache/module.go
@@ -1,7 +1,10 @@
 package cache
 
 import (
+	log "github.com/sirupsen/logrus"
+
 	"github.com/qbox/livekit/core/application"
+	"github.com/qbox/livekit/core/config"
 )
 
 const moduleName = "cache"
@@ -12,4 +15,22 @@ func init() {
 
 type Module struct {
 	application.EmptyModule
+}
+
+func (m *Module) Config(c *config.Config) error {
+	if c == nil {
+		return Nil
+	}
+
+	conf := Config{}
+	if err := c.Unmarshal(&conf); err != nil {
+		log.Error("Unmarshal config error %v", err)
+		return err
+	}
+
+	if err := Init(&conf); err != nil {
+		return err
+	}
+
+	return nil
 }
--- module/store/mysql/module.go
+++ module/store/mysql/module.go
@@ -1,1 +1,34 @@
 package mysql
+
+import (
+	"github.com/qbox/livekit/core/application"
+	"github.com/qbox/livekit/core/config"
+)
+
+const moduleName = "mysql"
+
+func init() {
+	application.RegisterModule(moduleName, &Module{})
+}
+
+type Module struct {
+	application.EmptyModule
+}
+
+func (m *Module) Config(c *config.Config) error {
+	if c == nil {
+		return nil
+	}
+
+	conf := &Config{}
+	if err := c.Unmarshal(&conf); err != nil {
+		return err
+	}
+
+	if len(conf.Databases) == 0 {
+		return nil
+	}
+
+	Init(conf.Databases...)
+	return nil
+}
