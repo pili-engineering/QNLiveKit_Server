d636ff51cd49ba63ea118c501771492af6e3756d
wangsheng <wangsheng@qiniu.com>
2022-09-23 09:12:20 +0800

    处理返回与异常

--- core/module/httpq/handle.go
+++ core/module/httpq/handle.go
@@ -1,20 +1,53 @@
 package httpq
 
 import (
+	"fmt"
 	"net/http"
+	"runtime"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/qbox/livekit/core/rest"
+	"github.com/qbox/livekit/utils/logger"
 )
 
 type HandlerFunc func(ctx *gin.Context) (interface{}, error)
 
 func makeHandle(f HandlerFunc) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ret, err := f(ctx)
-		if err != nil {
-			return
-		}
+		log := logger.ReqLogger(ctx)
 
-		ctx.JSON(http.StatusOK, ret)
+		var err error = nil
+		var ret interface{} = nil
+
+		defer func() {
+			if err != nil {
+
+			} else {
+				resp := &rest.Response{
+					RequestId: log.ReqID(),
+					Code:      0,
+					Message:   "",
+					Data:      ret,
+				}
+				ctx.JSON(http.StatusOK, resp)
+			}
+		}()
+
+		defer func() {
+			if r := recover(); r != nil {
+				const size = 16 << 10
+				buf := make([]byte, size)
+				buf = buf[:runtime.Stack(buf, false)]
+				log.Error("panic: ", r, fmt.Sprintf("\n%s", buf))
+
+				if _, ok := r.(error); ok {
+					err = r.(error)
+				} else {
+					err = fmt.Errorf("%v", r)
+				}
+			}
+		}()
+		ret, err = f(ctx)
 	}
 }
