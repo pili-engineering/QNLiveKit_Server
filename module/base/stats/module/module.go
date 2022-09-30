cd507a68e21c9dd019429b76744634387fcc7c7a
wangsheng <wangsheng@qiniu.com>
2022-09-30 14:39:09 +0800

    cherry-pick SOL-345

---
+++ module/base/stats/module/module.go
@@ -0,0 +1,29 @@
+package module
+
+import (
+	"github.com/qbox/livekit/core/application"
+	"github.com/qbox/livekit/core/config"
+	"github.com/qbox/livekit/module/base/stats/internal/controller/client"
+	"github.com/qbox/livekit/module/base/stats/internal/impl"
+	"github.com/qbox/livekit/module/base/stats/service"
+)
+
+const moduleName = "stats"
+
+func init() {
+	application.RegisterModule(moduleName, &Module{})
+}
+
+type Module struct {
+	application.EmptyModule
+}
+
+func (m *Module) Config(c *config.Config) error {
+	service.Instance = impl.NewServiceImpl()
+	return nil
+}
+
+func (m *Module) PreStart() error {
+	client.RegisterRoutes()
+	return nil
+}
--- module/base/stats/service/service.go
+++ module/base/stats/service/service.go
@@ -1,26 +1,19 @@
-package report
+package service
 
 import (
 	"context"
 
 	"github.com/qbox/livekit/biz/model"
+	"github.com/qbox/livekit/module/base/stats/internal/impl"
 )
 
-type RService interface {
+type IService interface {
 	ReportOnlineMessage(ctx context.Context)
 	PostStatsSingleLive(context.Context, []*model.StatsSingleLiveEntity) error
-	GetStatsSingleLive(ctx context.Context, liveId string) (*CommonStats, error)
+	GetStatsSingleLive(ctx context.Context, liveId string) (*impl.CommonStats, error)
 	UpdateSingleLive(ctx context.Context, entity *model.StatsSingleLiveEntity) error
 
 	SaveStatsSingleLive(ctx context.Context, entities []*model.StatsSingleLiveEntity) error
 }
 
-var service RService
-
-func GetService() RService {
-	return service
-}
-
-func InitService() {
-	service = NewReportClient()
-}
+var Instance IService
