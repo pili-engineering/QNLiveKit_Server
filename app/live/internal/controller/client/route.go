536acb9894e04af8aa68606ad9dbac9eb12ce78d
wangsheng <wangsheng@qiniu.com>
2022-09-30 11:39:29 +0800

    app config

--- app/live/internal/controller/client/route.go
+++ app/live/internal/controller/client/route.go
@@ -13,11 +13,12 @@ import (
 	"github.com/qbox/livekit/module/base/auth/internal/middleware"
 
 	"github.com/qbox/livekit/app/live/internal/config"
+	"github.com/qbox/livekit/core/module/appinfo/internal/controller"
 )
 
 func RegisterRoute(engine *gin.Engine) {
 	clientGroup := engine.Group("/client", middleware.AuthHandleFunc(config.AppConfig.JwtKey))
-	RegisterAppRoutes(clientGroup)
+	controller.RegisterAppRoutes(clientGroup)
 	RegisterMicRoutes(clientGroup)
 	RegisterStatsRoutes(clientGroup)
 }
