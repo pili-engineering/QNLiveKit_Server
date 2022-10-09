6e07e23ca8ddfebbe95a3d8e5ac0b36d010a0e89
wangsheng <wangsheng@qiniu.com>
2022-10-09 11:05:48 +0800

    加载所有的module

--- app/live/main.go
+++ app/live/main.go
@@ -12,6 +12,22 @@ import (
 	_ "net/http/pprof"
 
 	"github.com/qbox/livekit/core/application"
+	_ "github.com/qbox/livekit/module/base/auth/module"
+	_ "github.com/qbox/livekit/module/base/callback"
+	_ "github.com/qbox/livekit/module/base/live/module"
+	_ "github.com/qbox/livekit/module/base/stats/module"
+	_ "github.com/qbox/livekit/module/base/user/module"
+	_ "github.com/qbox/livekit/module/biz/censor/module"
+	_ "github.com/qbox/livekit/module/biz/gift/module"
+	_ "github.com/qbox/livekit/module/biz/item/module"
+	_ "github.com/qbox/livekit/module/biz/mic/module"
+	_ "github.com/qbox/livekit/module/biz/relay/module"
+	_ "github.com/qbox/livekit/module/extend/prome"
+	_ "github.com/qbox/livekit/module/fun/im"
+	_ "github.com/qbox/livekit/module/fun/pili"
+	_ "github.com/qbox/livekit/module/fun/rtc"
+	_ "github.com/qbox/livekit/module/store/cache"
+	_ "github.com/qbox/livekit/module/store/mysql"
 )
 
 var confPath = flag.String("f", "", "live -f /path/to/config")
