e1e97aa4bf9a7384d07617fcc13cf2acda4662ff
Y-SYN <36581904+Y-SYN@users.noreply.github.com>
2022-12-07 13:13:00 +0800

    返回json格式统一 (#92)

--- module/base/live/dto/live.go
+++ module/base/live/dto/live.go
@@ -13,28 +13,28 @@ import (
 )
 
 type LiveInfoDto struct {
-	LiveId       string          `json:"live_id"`
-	Title        string          `json:"title"`
-	Notice       string          `json:"notice"`
-	CoverUrl     string          `json:"cover_url"`
-	Extends      model.Extends   `json:"extends"`
-	AnchorInfo   userDto.UserDto `json:"anchor_info"`
-	AnchorStatus model.LiveRoomUserStatus
-	RoomToken    string `json:"room_token"`
-	PkId         string `json:"pk_id"`
-	OnlineCount  int    `json:"online_count"`
-	StartTime    int64  `json:"start_time"`
-	EndTime      int64  `json:"end_time"`
-	ChatId       int64  `json:"chat_id"`
-	PushUrl      string `json:"push_url"`
-	HlsUrl       string `json:"hls_url"`
-	RtmpUrl      string `json:"rtmp_url"`
-	FlvUrl       string `json:"flv_url"`
-	Pv           int    `json:"pv"`
-	Uv           int    `json:"uv"`
-	TotalCount   int    `json:"total_count"`
-	TotalMics    int    `json:"total_mics"`
-	LiveStatus   int    `json:"live_status"`
+	LiveId       string                   `json:"live_id"`
+	Title        string                   `json:"title"`
+	Notice       string                   `json:"notice"`
+	CoverUrl     string                   `json:"cover_url"`
+	Extends      model.Extends            `json:"extends"`
+	AnchorInfo   userDto.UserDto          `json:"anchor_info"`
+	AnchorStatus model.LiveRoomUserStatus `json:"anchor_status"`
+	RoomToken    string                   `json:"room_token"`
+	PkId         string                   `json:"pk_id"`
+	OnlineCount  int                      `json:"online_count"`
+	StartTime    int64                    `json:"start_time"`
+	EndTime      int64                    `json:"end_time"`
+	ChatId       int64                    `json:"chat_id"`
+	PushUrl      string                   `json:"push_url"`
+	HlsUrl       string                   `json:"hls_url"`
+	RtmpUrl      string                   `json:"rtmp_url"`
+	FlvUrl       string                   `json:"flv_url"`
+	Pv           int                      `json:"pv"`
+	Uv           int                      `json:"uv"`
+	TotalCount   int                      `json:"total_count"`
+	TotalMics    int                      `json:"total_mics"`
+	LiveStatus   int                      `json:"live_status"`
 
 	StopReason string `json:"stop_reason,omitempty"`  //关闭原因：censor 内容违规
 	StopUserId string `json:"stop_user_id,omitempty"` //关闭直播的管理员用户ID
@@ -52,7 +52,9 @@ func BuildLiveDto(liveEntity *model.LiveEntity, userEntity *model.LiveUserEntity
 	ret.RoomToken = ""
 	ret.PkId = liveEntity.PkId
 	ret.OnlineCount = liveEntity.OnlineCount
-	ret.StartTime = liveEntity.StartAt.Unix()
+	if liveEntity.StartAt != nil {
+		ret.StartTime = liveEntity.StartAt.Unix()
+	}
 	ret.EndTime = liveEntity.EndAt.Unix()
 	ret.ChatId = liveEntity.ChatId
 	ret.PushUrl = liveEntity.PushUrl
